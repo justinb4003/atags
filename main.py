beab6d50e6d795d63fd8f6905ff35f1eaed21762
Justin Buist <jbuist@debt480s.gigabuist.lan>
2023-01-12 14:12:41 -0500

    Post estimation added

--- main.py
+++ main.py
@@ -1,6 +1,57 @@
 import cv2
 import code
+import numpy
 import apriltag
+
+def draw_pose(overlay, camera_params, tag_size, pose, z_sign=1):
+
+    opoints = numpy.array([
+        -1, -1, 0,
+         1, -1, 0,
+         1,  1, 0,
+        -1,  1, 0,
+        -1, -1, -2*z_sign,
+         1, -1, -2*z_sign,
+         1,  1, -2*z_sign,
+        -1,  1, -2*z_sign,
+    ]).reshape(-1, 1, 3) * 0.5*tag_size
+
+    edges = numpy.array([
+        0, 1,
+        1, 2,
+        2, 3,
+        3, 0,
+        0, 4,
+        1, 5,
+        2, 6,
+        3, 7,
+        4, 5,
+        5, 6,
+        6, 7,
+        7, 4
+    ]).reshape(-1, 2)
+        
+    fx, fy, cx, cy = camera_params
+
+    K = numpy.array([fx, 0, cx, 0, fy, cy, 0, 0, 1]).reshape(3, 3)
+
+    rvec, _ = cv2.Rodrigues(pose[:3,:3])
+    tvec = pose[:3, 3]
+
+    dcoeffs = numpy.zeros(5)
+
+    ipoints, _ = cv2.projectPoints(opoints, rvec, tvec, K, dcoeffs)
+
+    ipoints = numpy.round(ipoints).astype(int)
+    
+    ipoints = [tuple(pt) for pt in ipoints.reshape(-1, 2)]
+
+    for i, j in edges:
+        cv2.line(overlay, ipoints[i], ipoints[j], (0, 255, 0), 1, 16)
+
+# t480s properties
+fx, fy, cx, cy = (9973.977159660377, 9432.229890251116, 470.03687743610527, 239.154470602995)
+camera_params = (fx, fy, cx, cy)
 
 cap = cv2.VideoCapture(0)
 
@@ -11,17 +62,17 @@ while True:
     
     detector = apriltag.Detector(options)
     results = detector.detect(gray)
-    output = cv2.cvtColor(src=orig, code=cv2.COLOR_BGR2RGB)
+    output = orig # cv2.cvtColor(src=orig, code=cv2.COLOR_BGR2RGB)
     
     # loop over the AprilTag detection results
     # print(f'Tags found: {len(results)}')
     for r in results:
+        pose, e0, e1 = detector.detection_pose(r, camera_params, 1, 1)
         # skip low confidence images
-        if r.decision_margin < 30 or r.hamming > 0:
+        draw_pose(output, camera_params, 1, pose, 1)
+        if r.decision_margin < 30 or r.hamming > 4:
             continue
         print(r.tag_id)
-        if r.tag_id != 1:
-            code.interact(local=locals())
         # extract the bounding box (x, y)-coordinates for the AprilTag
         # and convert each of the (x, y)-coordinate pairs to integers
         (ptA, ptB, ptC, ptD) = r.corners
@@ -40,7 +91,7 @@ while True:
         
 
         # Label it with something for now
-        tagFamily = str(r.decision_margin)
+        tagFamily = str(r.tag_id)
         cv2.putText(output, tagFamily, (ptA[0], ptA[1] - 15),
             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
         print("[INFO] tag family: {}".format(tagFamily))
